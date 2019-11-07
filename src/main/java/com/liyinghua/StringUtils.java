83c81e5cdd07ed4c1664d9c0eac79f843098ea32
liyinghua <1483818379@qq.com>
2019-11-07 19:31:00 +0800

    本地修改

--- src/main/java/com/liyinghua/StringUtils.java
+++ src/main/java/com/liyinghua/StringUtils.java
@@ -9,7 +9,13 @@ public class StringUtils {
 	 * @return
 	 * @return: boolean
 	 */
+	//本地修改
 		public static boolean isBlank(String str) {
 			return (str==null||str.trim().equals("")) ;
 }
+	public static boolean isMobile(String str) {
+		
+		return false;
+		
+	}
 }
