62a6e8738d8905a97c9312c6e0d42675fc3d6708
liyinghua <1483818379@qq.com>
2019-11-07 15:30:25 +0800

    完成字符串的非空判断

--- src/main/java/com/liyinghua/StringUtils.java
+++ src/main/java/com/liyinghua/StringUtils.java
@@ -1,5 +1,15 @@
 package com.liyinghua;
 
 public class StringUtils {
-
+	/**
+	 * 
+	 * @Title: isBlank 
+	 * @Description: 判断字符串是否为空
+	 * @param str
+	 * @return
+	 * @return: boolean
+	 */
+		public static boolean isBlank(String str) {
+			return (str==null||str.trim().equals("")) ;
+}
 }
