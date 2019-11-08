ff02dbc52fd37d929c975d0452e1a3d0c7aba0da
liyinghua <1483818379@qq.com>
2019-11-08 09:58:13 +0800

    添加时间工具类

--- src/main/java/com/liyinghua/StringUtils.java
+++ src/main/java/com/liyinghua/StringUtils.java
@@ -1,5 +1,8 @@
 package com.liyinghua;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class StringUtils {
 	/**
 	 * 
@@ -12,4 +15,46 @@ public class StringUtils {
 		public static boolean isBlank(String str) {
 			return (str==null||str.trim().equals("")) ;
 }
+		/**
+		 * 
+		 * @Title: getDateByInitMonth 
+		 * @Description: 返回传入日期的月初
+		 * @param src
+		 * @return
+		 * @return: Date
+		 */
+		public static Date getDateByInitMonth(Date src) {
+			 //获取日历类
+			Calendar c = Calendar.getInstance();
+			//根据传入的日期构建日历类
+			c.setTime(src);
+			//设置月初
+			c.set(Calendar.DAY_OF_MONTH, 1);//日
+			c.set(Calendar.HOUR_OF_DAY, 0);//小时
+			c.set(Calendar.MINUTE, 0);//分钟
+			c.set(Calendar.SECOND, 0);//秒	
+			return c.getTime();
+		}
+		/**
+		 * 
+		 * @Title: getDateByFullMonth 
+		 * @Description: 获取传入日期的最后一秒
+		 * @param src
+		 * @return
+		 * @return: Date
+		 */
+		public static Date  getDateByFullMonth(Date src) {
+			Calendar c = Calendar.getInstance();
+			c.setTime(src);
+			//让月份加一
+			c.add(Calendar.MONTH, 1);
+			//让日期变成月初
+			Date date = getDateByInitMonth(c.getTime());
+			//让月初初始化日历类
+			c.setTime(date);
+			//减一
+			c.add(Calendar.SECOND, -1);
+			
+			return c.getTime();
+		}
 }
--- src/test/java/com/lyh/text/TestStringUtils.java
+++ src/test/java/com/lyh/text/TestStringUtils.java
@@ -3,9 +3,13 @@ package com.lyh.text;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Test;
 
 import com.liyinghua.StringUtils;
+
 
 public class TestStringUtils {
 	@Test
@@ -13,5 +17,21 @@ public class TestStringUtils {
 		//断言测试
 		assertFalse(StringUtils.isBlank("aaa"));
 		assertTrue(StringUtils.isBlank(" "));
+	}
+	@Test
+	public void textGetDateByInitMonth() {
+		//获取更改后时间
+		Date date = StringUtils.getDateByInitMonth(new Date());
+		//设置格式
+		SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		//输出
+		System.out.println(df.format(date));
+	}
+	@Test
+	public void textGetDateByFullMonth() {
+		
+		Date date = StringUtils.getDateByFullMonth(new Date());
+		SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println(df.format(date));
 	}
 }
